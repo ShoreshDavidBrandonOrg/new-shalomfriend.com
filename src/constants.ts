d48625a8941bcf3ec8e962472ec9efb5efea9f6f
Clayton Ray <iamclaytonray@gmail.com>
2021-09-26 13:37:35 -0400

    fix: api url

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 const LOCAL_API = 'http://localhost:3000/v2';
-const PROD_API = 'https://api.shalomfriend.com/v1';
+const PROD_API = 'https://shalomfriend-production.herokuapp.com/v2';
 
 const nodeEnv = process.env.NODE_ENV;
 
-export const API_URL = nodeEnv === 'production' ? PROD_API : LOCAL_API;
+export const API_URL = nodeEnv !== 'production' ? LOCAL_API : PROD_API;
